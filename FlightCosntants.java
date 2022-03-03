3d0616157aea66286d1a1fb8b83464f75af57bd9
davwamai <davwamai@gmail.com>
2022-03-03 12:43:01 -0500

    added hotel funcs

--- FlightCosntants.java
+++ FlightCosntants.java
@@ -2,6 +2,6 @@ package things;
 
 public class FlightCosntants {
     protected static final String FLIGHTS_FILE_NAME = "things/flights.json";
-    protected static final String PASSENGER_UID = "id";
-    protected static final String PASSENGER_USER_NAME = "username";
+    protected static final String PASSENGER_UID = "flight-id";
+    
 }
--- HotelConstants.java
+++ HotelConstants.java
@@ -1,5 +1,12 @@
 package things;
 
 public class HotelConstants {
-    
+    protected static final String HOTEL_NAME = "name";
+    protected static final String HOTEL_ID = "id";
+    protected static final String HOTEL_LOCATION = "location";
+    protected static final String HOTEL_CITY = "city";
+    protected static final String HOTEL_COUNTRY = "country";
+    protected static final String HOTEL_CONTINENT = "continent";
+    protected static final String HOTEL_VACANCY = "vacancy";
+    protected static final String NUM_HOTEL_ROOMS = "numRooms";
 }
